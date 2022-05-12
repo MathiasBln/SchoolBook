c725bcc7196acc46f90e53ba4af5e1caf7cb3bec
Martin Claver <m.claver99@gmail.com>
2022-05-12 18:23:33 +0200

    modif profil

--- profil.php
+++ profil.php
@@ -1,4 +1,5 @@
 <?php
+// $id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
 $engine = "mysql";
 $host = "localhost";
 $port = 3306;
@@ -6,31 +7,37 @@ $dbname = "db_schoolbook";
 $username = "root";
 $password = "root";
 $pdo = new PDO("$engine:host=$host:$port;dbname=$dbname", $username, $password);
-$maRequete = $pdo->prepare("SELECT * FROM users");
-// Etape 2 : On exécute la requête
+$maRequete = $pdo->prepare("SELECT * FROM users WHERE iduser = 1");
+// $maRequete->execute([
+//     ":id" => $id
+// ]);
 $maRequete->execute();
-// Etape 3 : Si c'est un SELECT, on récupère LE (fetch) ou LES (fetchAll) résultatS
-// PDO::FETCH_ASSOC va ramener uniquement les noms de colonnes
 $user = $maRequete->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="profil.css">
-    <title>Profil <?php $user["name"]." ".$user["last_name"] ?> </title>
+    <link rel="stylesheet" href="css/profil.css">
+    <?php foreach($user as $element){ ?>
+
+    <title> Profil de <?php echo($element["name"]." ".$element["last_name"]); ?> </title>
 </head>
 <body>
     <div class="banner">
         <img src="" alt="">
     </div>
-
     <img class="profile_pic" src="" alt="">
 
-    <h1><?php $user["name"]." ".$user["last_name"] ?></h1>
-    <h2><?php $user["school_id"]?></h2>
+    
+    <h1><?php  echo($element["name"]." ".$element["last_name"]); ?></h1>
+    <h2><?= $element["school_id"]; }; ?></h2>
+    
+
 
 
 </body>
