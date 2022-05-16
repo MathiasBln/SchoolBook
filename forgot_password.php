209f05878384893b3c0bfac5e8229f4658a7042c
Farmata-sidibe <69140880+Farmata-sidibe@users.noreply.github.com>
2022-05-16 10:18:13 +0200

    forget password

--- forgot_password.php
+++ forgot_password.php
@@ -1,104 +1,54 @@
 <?php
-
- /*ecécuter cette ligne dans votre terminal pour intallez phpmailer composer require phpmailer/phpmailer */
-  require_once 'configuration.php';
-  if(isset($_POST['validate'])){
-    if (!empty($_POST['email'])){
-        $email = htmlspecialchars($_POST['email']);
-        $password = uniqid();
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-            
-        // phpmailer qui va permettre d'envoie un mail au users
-        function smtpmailer($to, $from, $from_name, $subject, $body){
-            $email = htmlspecialchars($_POST['email']);
-            $mail = new PHPMailer();
-            $mail->IsSMTP();
-            $mail->SMTPAuth = true; 
-    
-            $mail->SMTPSecure = 'ssl'; 
-            $mail->Host = 'smtp.gmail.com';
-            $mail->Port = 465;
-            $mail->Username = 'projetsidibe1@gmail.com';
-            $mail->Password = 'Projetsidibe@';   
-    
-            //   $path = 'reseller.pdf';
-            //   $mail->AddAttachment($path);
-    
-            $mail->IsHTML(true);
-            $mail->From="projetsidibe1@gmail.com";
-            $mail->FromName=$from_name;
-            $mail->Sender=$from;
-            $mail->AddReplyTo($from, $from_name);
-            $mail->Subject = $subject;
-            $mail->Body = $body;
-            $mail->AddAddress($to);
-        
-            if ($mail->Send()) {
-                $stmt = $pdo->prepare("UPDATE users SET password = ? WHERE email = ?");
-                $stmt->execute([$hashedPassword, $_POST['email']]);
-                echo "E-mail envoyé";
-            } else {
-                echo "Une erreur est survenue";
-                var_dump($email);
-                
-            } 
-        
-        }
-            
-        $to = $email;
-        $from = 'projetsidibe1@gmail.com';
-        $name = 'SchoolBook';
-        $subj = 'Recover your password';
-        $msg = "Bonjour, voici votre nouveau mot de passe : $password";
-            
-        $error=smtpmailer($to,$from, $name ,$subj, $msg);
-    }
-  }
-   
+    require_once 'configuration.php';
 
 ?>
-
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>forget pwd</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style-index.css">
+    <title>SchoolBook</title>
 </head>
-
 <body>
-    <h2>Forgot password</h2>
-    <form action="forgot_password" method="post">
-        <div class="container">
-            <label for="email"><b>Email</b></label>
-            <input type="email" placeholder="Enter Email" name="email" required>
-            <button type="submit" name="validate">Send me a random password</button>
-        </div>
-    </form>
-</body>
+    <div class="login-form">
 
+    <form method="post" class="form2">
+        <div class="text-log">
+        <h2>Reset password</h2>
+        <p>An e-mail will be send to you with instructions on how to reset your password.</p>
+        </div>
+
+        <div class="inputs">
+        <input type="email" class="form-input" placeholder=" |Enter your email..." name="email" required="required" autocomplete="off">
+        
+        </div>
+        <button type="submit"  class="btn-login">Send me a random password</button>
+    </form>
+    </div>
+    </body>
 </html>
 
-<!--  $password = uniqid();
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    <?php
+   if (isset($_POST['email'])) {
+       $password = uniqid();
+       $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-        $subject = 'Mot de passe oublié';
-        $message = "Bonjour, voici votre nouveau mot de passe : $password";
-        $headers = 'Content-Type: text/plain; charset="UTF-8"';
+       $message = "Bonjour, voici votre nouveau mot de passe : $password";
+       $headers = 'content-type: text/plain; charset="utf-8"'." ";
 
-        if (mail($_POST['email'], $subject, $message, $headers)) {
-            $stmt = $pdo->prepare("UPDATE users SET password = ? WHERE email = ?");
-            $stmt->execute([$hashedPassword, $_POST['email']]);
-            echo "E-mail envoyé";
-        } else {
-            echo "Une erreur est survenue";
-        } 
-    
-     $recupUser = $pdo->prepare('SELECT * FROM users WHERE email = ?');
-        $recupUser->execute(array($email));
-        if($recupUser->rowCount()>0){
-            $userInfos= $recupUser->fetch();
-            $_SESSION['id'] = $userInfos['id'];
-    
-    
-    -->
+       if (mail($_POST['email'], 'Mot de passe oublié', $message, $headers)) {
+           $sql = "UPDATE users SET password = ? WHERE email = ?";
+           $stmt = $pdo->prepare($sql);
+           $stmt->execute([$hashedPassword, $_POST['email']]);
+           echo "Mail envoyé";
+       } else {
+           echo "une erreur es survenue ...";
+       }
+    }
+
+        
+
+
+
